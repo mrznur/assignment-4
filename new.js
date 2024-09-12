54d0c5676254e8cb956291330a530d94c582c561
mrznur <mahmudunmiraz@gmail.com>
2024-09-13 00:18:15 +0600

    commiting

---
+++ new.js
@@ -0,0 +1,93 @@
+function calculateTax(income, expenses) {
+    if (income < 0 || expenses < 0 || expenses > income) {
+        console.log("Invalid Input");
+    } else {
+        let leftoverMoney = income - expenses;
+        let tax = 0.2 * leftoverMoney;
+        return tax;
+    }
+};
+
+function sendNotification(email) {
+    if (email.indexOf('@') === -1) {
+        return "Invalid Email";
+    }
+
+    let emailParts = email.split('@');
+    let userName = emailParts[0];
+    let emailDomain = emailParts[1];
+
+    return userName + " sent you an email from " + emailDomain;
+};
+
+function checkDigitsInName(name) {
+    if (typeof name !== 'string') {
+        return "Invalid Input";
+    }
+
+    for (let index = 0; index < name.length; index++) {
+        if (name[index] >= '0' && name[index] <= '9') {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+function calculateFinalScore(obj) {
+    if (typeof obj !== 'object' || obj === null || Array.isArray(obj)) {
+        return "Invalid Input";
+    }
+
+    let { testScore, schoolGrade, isFFamily } = obj;
+
+    if (typeof testScore !== 'number' || testScore > 50 || typeof schoolGrade !== 'number' || schoolGrade > 30 || typeof isFFamily !== 'boolean') {
+        return "Invalid Input";
+    }
+
+    let totalScore = testScore + schoolGrade;
+
+    if (isFFamily) {
+        totalScore += 20;
+    }
+
+    return totalScore >= 80;
+};
+
+function waitingTime(waitingTimes, serialNumber) {
+    if (!Array.isArray(waitingTimes) || typeof serialNumber !== 'number') {
+        return "Invalid Input";
+    }
+
+    let sumOfTimes = 0;
+    for (let i = 0; i < waitingTimes.length; i++) {
+        sumOfTimes += waitingTimes[i];
+    }
+
+    let avgWaitTime = Math.round(sumOfTimes / waitingTimes.length);
+    let peopleLeft = serialNumber - waitingTimes.length - 1;
+
+    if (peopleLeft <= 0) {
+        return 0;
+    }
+
+    let totalWaitTime = avgWaitTime * peopleLeft;
+
+    return totalWaitTime;
+};
+
+console.log(calculateTax(5000, 5000));
+
+console.log(sendNotification("zihad.ph@gmail.com"));
+
+console.log(checkDigitsInName("Raj123"));
+
+let student1 = {
+    testScore: 45,
+    schoolGrade: 25,
+    isFFamily: true
+};
+
+console.log(calculateFinalScore(student1));
+
+console.log(waitingTime([3,5,7,11,6],10));
